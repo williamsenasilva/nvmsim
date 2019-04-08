0969ace4a73dc8cda119b52ea2cd8a94662278e2
William Sena Silva <william.sena@tecsidel.com>
2019-04-07 22:22:31 -0300

    Atualizado versão do gcc

--- shared/run-sniper-spec-tests.sh
+++ shared/run-sniper-spec-tests.sh
@@ -2,12 +2,12 @@
 
 DATE=`date '+%Y-%m-%d-%Hh%Mm%Ss'`
 
-cd /opt/spec_cpu2006/
+cd /opt/spec_cpu2006/ 
 . ./shrc 
 cd /nvmsim/ 
 mkdir -p ./logs/sniper/spec/$DATE 
 mkdir -p ./logs/sniper/spec/$DATE/logs
 
 echo "running..."
-run-sniper -d ./logs/sniper/spec/$DATE/logs -- /opt/spec_cpu2006/bin/runspec --config=linux64-amd64-gcc63.cfg --test
+run-sniper -d ./logs/sniper/spec/$DATE/logs -- /opt/spec_cpu2006/bin/runspec --config=linux64-amd64-gcc73.cfg --size=test --tune=base --noreportable --iterations=1 bzip2
 echo "running... done"

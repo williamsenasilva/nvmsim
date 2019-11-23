374f27e50d1d8166384e2c43f006bb5ba2b60f93
William Sena Silva <williamsenasilva@yahoo.com>
2019-11-23 09:58:27 -0300

    inicializado uso de socket na classe da nvm

--- src/sniper/dram_perf_model_nvm.cc
+++ src/sniper/dram_perf_model_nvm.cc
@@ -4,9 +4,19 @@
 #include "config.hpp"
 #include "stats.h"
 #include "shmem_perf.h"
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
 
-DramPerfModelNVM::DramPerfModelNVM(core_id_t core_id,
-                                             UInt32 cache_block_size):
+#define MAX 80 
+#define PORT 2000
+
+DramPerfModelNVM::DramPerfModelNVM(core_id_t core_id, UInt32 cache_block_size):
         DramPerfModel(core_id, cache_block_size),
         m_queue_model(NULL),
         m_dram_bandwidth(8 * Sim()->getCfg()->getFloat("perf_model/dram/per_controller_bandwidth")), // Convert bytes to bits
@@ -17,8 +27,11 @@ DramPerfModelNVM::DramPerfModelNVM(core_id_t core_id,
 
     if (Sim()->getCfg()->getBool("perf_model/dram/queue_model/enabled"))
     {
-        m_queue_model = QueueModel::create("dram-queue", core_id, Sim()->getCfg()->getString("perf_model/dram/queue_model/type"),
-                                           m_dram_bandwidth.getRoundedLatency(8 * cache_block_size)); // bytes to bits
+        m_queue_model = QueueModel::create(
+            "dram-queue", 
+            core_id, 
+            Sim()->getCfg()->getString("perf_model/dram/queue_model/type"),
+            m_dram_bandwidth.getRoundedLatency(8 * cache_block_size)); // bytes to bits
     }
 
     registerStatsMetric("dram", core_id, "total-access-latency", &m_total_access_latency);
@@ -35,43 +48,139 @@ DramPerfModelNVM::~DramPerfModelNVM()
     }
 }
 
-SubsecondTime
-DramPerfModelNVM::getAccessLatency(SubsecondTime pkt_time, UInt64 pkt_size, core_id_t requester, IntPtr address, DramCntlrInterface::access_t access_type, ShmemPerf *perf)
+SubsecondTime DramPerfModelNVM::getAccessLatency(SubsecondTime pkt_time, UInt64 pkt_size, core_id_t requester, IntPtr address, DramCntlrInterface::access_t access_type, ShmemPerf *perf)
 {
-    // pkt_size is in 'Bytes'
-    // m_dram_bandwidth is in 'Bits per clock cycle'
-    if ((!m_enabled) ||
-        (requester >= (core_id_t) Config::getSingleton()->getApplicationCores()))
-    {
-        return SubsecondTime::Zero();
-    }
+   // pkt_size is in 'Bytes'
+   // m_dram_bandwidth is in 'Bits per clock cycle'
+   if ((!m_enabled) || (requester >= (core_id_t) Config::getSingleton()->getApplicationCores()))
+   {
+      return SubsecondTime::Zero();
+   }
 
-    SubsecondTime processing_time = m_dram_bandwidth.getRoundedLatency(8 * pkt_size); // bytes to bits
+   SubsecondTime processing_time = m_dram_bandwidth.getRoundedLatency(8 * pkt_size); // bytes to bits
 
-    // Compute Queue Delay
-    SubsecondTime queue_delay;
-    if (m_queue_model)
-    {
-        queue_delay = m_queue_model->computeQueueDelay(pkt_time, processing_time, requester);
-    }
-    else
-    {
-        queue_delay = SubsecondTime::Zero();
-    }
+   // Compute Queue Delay
+   SubsecondTime queue_delay;
+   if (m_queue_model)
+   {
+      queue_delay = m_queue_model->computeQueueDelay(pkt_time, processing_time, requester);
+   }
+   else
+   {
+      queue_delay = SubsecondTime::Zero();
+   }
 
-    SubsecondTime access_latency = queue_delay + processing_time + m_dram_access_cost;
+   SubsecondTime access_latency = queue_delay + processing_time + m_dram_access_cost;
 
 
-    perf->updateTime(pkt_time);
-    perf->updateTime(pkt_time + queue_delay, ShmemPerf::DRAM_QUEUE);
-    perf->updateTime(pkt_time + queue_delay + processing_time, ShmemPerf::DRAM_BUS);
-    perf->updateTime(pkt_time + queue_delay + processing_time + m_dram_access_cost, ShmemPerf::DRAM_DEVICE);
+   perf->updateTime(pkt_time);
+   perf->updateTime(pkt_time + queue_delay, ShmemPerf::DRAM_QUEUE);
+   perf->updateTime(pkt_time + queue_delay + processing_time, ShmemPerf::DRAM_BUS);
+   perf->updateTime(pkt_time + queue_delay + processing_time + m_dram_access_cost, ShmemPerf::DRAM_DEVICE);
 
-    // Update Memory Counters
-    m_num_accesses ++;
-    m_total_access_latency += access_latency;
-    m_total_queueing_delay += queue_delay;
+   // Update Memory Counters
+   m_num_accesses ++;
+   m_total_access_latency += access_latency;
+   m_total_queueing_delay += queue_delay;
 
-    printf("[NVMSIM] {num_accesses,access_latency,total_access_latency}: %" PRIu64  ",%" PRIu64 "ns,%" PRIu64 "ns\n", m_num_accesses, access_latency.getNS(), m_total_access_latency.getNS());
-    return access_latency;
+   printf("[NVMSIM] {num_accesses,access_latency,total_access_latency}: %" PRIu64  ",%" PRIu64 "ns,%" PRIu64 "ns\n", m_num_accesses, access_latency.getNS(), m_total_access_latency.getNS());
+   return access_latency;
 }
+
+SubsecondTime DramPerfModelNVM::getAccessLatency()
+{
+  int client_socket, client_connection; 
+   struct sockaddr_in servaddr, cli; 
+
+   if(argc < 2)
+   {
+      printf("Please provide a hostname to resolve\n");
+      exit(1);
+   }
+    
+   char *hostname = argv[1];
+   char ip[100];
+    
+   hostname_to_ip(hostname , ip);
+   printf("%s resolved to %s\n" , hostname , ip);
+
+   // socket create and varification 
+   client_socket = socket(AF_INET, SOCK_STREAM, 0); 
+   if (client_socket == -1) 
+   { 
+      printf("socket creation failed...\n"); 
+      exit(0); 
+   } 
+   else
+   {
+      printf("Socket successfully created..\n"); 
+   }
+   bzero(&servaddr, sizeof(servaddr)); 
+
+   // assign IP, PORT 
+   servaddr.sin_family = AF_INET; 
+   servaddr.sin_addr.s_addr = inet_addr(ip); 
+   servaddr.sin_port = htons(PORT); 
+
+   // connect the client socket to server socket 
+   if (connect(client_socket, (struct sockaddr*) &servaddr, sizeof(servaddr)) != 0) 
+   { 
+      printf("connection with the server failed...\n"); 
+      exit(0); 
+   } 
+   else
+   {
+      printf("connected to the server..\n"); 
+   }
+
+   // function for chat 
+   chat(client_socket); 
+
+   // close the socket 
+   close(client_socket);
+}
+
+int DramPerfModelNVM::getHostnameByIP(char * hostname , char* ip)
+{
+   struct hostent *he;
+   struct in_addr **addr_list;
+   int i;
+       
+   if ( (he = gethostbyname( hostname ) ) == NULL) 
+   {
+      // get the host info
+      herror("gethostbyname");
+      return 1;
+   }
+ 
+   addr_list = (struct in_addr **) he->h_addr_list;
+    
+   for(i = 0; addr_list[i] != NULL; i++) 
+   {
+      //Return the first one;
+      strcpy(ip , inet_ntoa(*addr_list[i]) );
+      return 0;
+   }
+
+   return 1;
+}
+
+void DramPerfModelNVM::chat(int client_socket) 
+{ 
+   char buffer[MAX]; 
+   
+   for (int calls = 1; calls <= 100; calls++) 
+   { 
+      bzero(buffer, sizeof(buffer)); 
+      sprintf(buffer, "%d", calls);
+      write(client_socket, buffer, sizeof(buffer)); 
+      bzero(buffer, sizeof(buffer)); 
+      read(client_socket, buffer, sizeof(buffer)); 
+      printf("From Server : %s\n", buffer);  
+   }
+
+   bzero(buffer, sizeof(buffer)); 
+   sprintf(buffer, "%s", "exit");
+   write(client_socket, buffer, sizeof(buffer)); 
+   printf("Client Exit...\n");
+} 
--- src/sniper/dram_perf_model_nvm.h
+++ src/sniper/dram_perf_model_nvm.h
@@ -13,17 +13,15 @@ private:
     QueueModel* m_queue_model;
     SubsecondTime m_dram_access_cost;
     ComponentBandwidth m_dram_bandwidth;
-
     SubsecondTime m_total_queueing_delay;
     SubsecondTime m_total_access_latency;
-
+    int getHostnameByIP(char* hostname, char* ip);
+    void chat(int client_socket);
 public:
-    DramPerfModelNVM(core_id_t core_id,
-                          UInt32 cache_block_size);
-
+    DramPerfModelNVM(core_id_t core_id, UInt32 cache_block_size);
     ~DramPerfModelNVM();
-
     SubsecondTime getAccessLatency(SubsecondTime pkt_time, UInt64 pkt_size, core_id_t requester, IntPtr address, DramCntlrInterface::access_t access_type, ShmemPerf *perf);
+    SubsecondTime getAccessLatency();
 };
 
 #endif /* __DRAM_PERF_MODEL_NVM_H__ */

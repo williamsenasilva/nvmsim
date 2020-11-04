6c6d2f5503c2430ba07d93b2e0356436d3657d4d
William Sena Silva <williamsenasilva@yahoo.com>
2020-11-03 22:11:11 -0300

    Zerado o conteúdo de data64. Substituído malloc por posix_memalign.

--- src/helper/c_programs/basic/basic.c
+++ src/helper/c_programs/basic/basic.c
@@ -10,32 +10,39 @@ void print_data64(String64 *, int);
 
 int main()
 {
-   int memory_size = 1048576; //128072; // 128KB // 1048576 (1MB)
+   int memory_size = 1048576; // (1MB)
    int cache_block_size = 64;
    int total_of_lines = memory_size / cache_block_size;
    int counter_filled_lines = 0;
    int n_random;
-   String64 *data64 = (String64 *) malloc(total_of_lines * sizeof(String64));
+   String64 *data64;
+
+   posix_memalign((void **) &data64, 64, total_of_lines * sizeof(String64));
 
    if(data64 == NULL)
       exit (1);
+
+   // clear data64
+   for(int i = 0; i < total_of_lines; i++)
+      for (int j = 0; j < cache_block_size; j++)
+         data64[i].s[j] = 0x00;
 
    srand(time(0));
 
    while(counter_filled_lines != total_of_lines)
    {
       n_random = rand() % total_of_lines;
-      if(data64[n_random].s[0] != 'f')
+      if(data64[n_random].s[0] == 0)
       {
          for (int j = 0; j < cache_block_size; j++)
          {
-            data64[n_random].s[j] = 'f';
+            data64[n_random].s[j] = 0xFF;
          }
          counter_filled_lines++;
       }
    }
 
-   //print_data64(data64, total_of_lines);
+   // print_data64(data64, total_of_lines);
 
    free(data64);
 
@@ -44,13 +51,13 @@ int main()
 
 void print_data64(String64 *data64, int total_of_lines)
 {
-   printf("size of String64: %d\n", sizeof(String64));
+   printf("size of String64: %lu\n", sizeof(String64));
    for(int i = 0; i < total_of_lines; i++)
    {
       printf("data64[%d] = ", i);
       for (int j = 0; j < 64; j++)
       {
-         printf("%c", data64[i].s[j]);
+         printf("%x", data64[i].s[j]);
       }
       printf("\n");
    }

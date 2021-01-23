d0d00d6eb9e4a2800704ef9357ba446b84fbe58b
William Sena Silva <williamsenasilva@yahoo.com>
2021-01-23 14:27:41 -0300

    Adicionado logs com cor nos scripts.

--- nvmsim.sh
+++ nvmsim.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-action=$1
-option=$2
+nvmsim_script_action=$1
+docker_compose_optional_arg=$2
+
 path=$(pwd)
-
-# enter in project folder
 cd $path
-
 source ${path}/.env
 
 sniper_path="${SNIPER_PATH:-}"
@@ -16,11 +14,13 @@ sniper_arch="${SNIPER_TARGET_ARCH:-intel64}"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW="\033[0;33m"
 NOCOLOR='\033[0m'
 
 echo -e "${NOCOLOR}[NVMSIM][INFO ] NVMSIM started."
 
-function copy_third_source_files() {
+function copy_third_source_files
+{
   if [ $sniper_path ] && [ $pinplay_path ] && [ $nvmain_path ]; then
     echo "[NVMSIM][INFO ] Copying artifacts... "
     rm -rf ${path}/shared/sniper
@@ -31,12 +31,14 @@ function copy_third_source_files() {
     cp -r ${nvmain_path} ${path}/shared/nvmain
     echo -e "[NVMSIM][INFO ] Copying artifacts... ${GREEN}OK${NOCOLOR}"
   else
-    echo "[NVMSIM][ERROR] You must define SNIPER_PATH, PINPLAY_PATH, and NVMAIN_PATH in your .env file."
+    echo -e "[NVMSIM][${RED}ERROR${NOCOLOR}] You must define SNIPER_PATH, PINPLAY_PATH, and NVMAIN_PATH in your .env file."
     exit 1
   fi
 }
 
-function copy_source_files() {
+function copy_source_files
+{
+  echo "[NVMSIM][INFO ] Copying source files... "
   # Sniper files
   cp ${path}/src/sniper/core.cc ${path}/shared/sniper/common/core/
   cp ${path}/src/sniper/cache_cntlr.cc ${path}/shared/sniper/common/core/memory_subsystem/parametric_dram_directory_msi/
@@ -57,7 +59,8 @@ function copy_source_files() {
   cp ${path}/src/sniper/dram_cntlr_interface.cc ${path}/shared/sniper/common/core/memory_subsystem/dram/
   cp ${path}/src/sniper/nvmsim.cfg ${path}/shared/sniper/config/
   # c program
-  gcc -O0 ${path}/src/helper/c_programs/basic/basic.c -o ${path}/shared/sniper/nvmsim
+  # gcc -O0 ${path}/src/helper/c_programs/single-line-cache.c -o ${path}/shared/sniper/nvmsim
+  gcc -O0 ${path}/src/helper/c_programs/multiple-line-cache.c -o ${path}/shared/sniper/nvmsim
   # NVMain files
   mkdir -p ${path}/shared/nvmain/traceReader/SniperTrace
   cp ${path}/src/nvmain/GenericTraceReader.h ${path}/shared/nvmain/traceReader
@@ -77,25 +80,30 @@ function copy_source_files() {
   cp ${path}/src/nvmain/traceMain.cpp ${path}/shared/nvmain/traceSim
   cp ${path}/src/nvmain/SConscript ${path}/shared/nvmain
   cp ${path}/src/nvmain/tracefile.nvt ${path}/shared/nvmsim
+  echo -e "[NVMSIM][INFO ] Copying source files... ${GREEN}OK${NOCOLOR}"
 }
 
-action=${action:-run}
-if [ $action == build ]; then
-  reset
+nvmsim_script_action=${nvmsim_script_action:-run}
+
+if [ $nvmsim_script_action == build-images ]; then
+  docker-compose down
+  docker-compose build ${docker_compose_optional_arg}
+elif [ $nvmsim_script_action == build-envs ]; then
   docker-compose down
   copy_third_source_files
   copy_source_files
   # remove gem5 dependency
   sed -i -e 's/from gem5_scons import Transform/#from gem5_scons import Transform/g' ${path}/shared/nvmain/SConscript
-  docker-compose ${action} ${option}
-elif [ $action == run ]; then
-  reset
+  docker-compose up -d && docker-compose logs -f
+elif [ $nvmsim_script_action == run ]; then
   docker-compose down
   copy_source_files
+  rm -rf ${path}/shared/nvmsim/logs/
   docker-compose up -d && docker-compose logs -f
-elif [ $action == stop ]; then
+elif [ $nvmsim_script_action == stop ]; then
   docker-compose down
 else
-  echo "[NVMSIM][ERROR] Parameter \"${action}\" not accepted. Please, use run, stop or build."
+  echo -e "[NVMSIM][${RED}ERROR${NOCOLOR}] Argument \"${nvmsim_script_action}\" not accepted."
+  echo "[NVMSIM][INFO ] Usage: nvmsim.sh [run, stop, build-images, build-envs]"
   exit 1
 fi

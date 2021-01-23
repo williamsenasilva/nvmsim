d7356a5f0823b2207be7f0e935e91cdc889d2e05
William Sena Silva <williamsenasilva@yahoo.com>
2021-01-23 14:18:28 -0300

    Adicionado logs com cor nos scripts. Adicionado programas úteis dentro nas imagens Docker.

--- src/entrypoints/entrypoint-nvmain.sh
+++ src/entrypoints/entrypoint-nvmain.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 echo "[NVMSIM][INFO ] NVMain Docker entrypoint"
-cd /opt/nvmain || exit 0
-scons --build-type=fast
-scons --build-type=prof
-scons --build-type=debug
-./nvmain.debug Config/2D_DRAM_example.config /mnt/nvmsim/tracefile 0 TraceReader=SniperTrace
+
+ENABLE_NVMAIN=${ENABLE_NVMAIN:-1}
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW="\033[0;33m"
+NOCOLOR='\033[0m'
+
+if [ $ENABLE_NVMAIN == 1 ]; then
+    cd /opt/nvmain || exit 0
+    scons --build-type=fast
+    scons --build-type=prof
+    scons --build-type=debug
+    ./nvmain.debug Config/2D_DRAM_example.config /mnt/nvmsim/tracefile 0 TraceReader=SniperTrace
+else
+    echo -e "[NVMSIM][${YELLOW}WARN ${NOCOLOR}] NVMain Docker entrypoint disabled"
+    tail -f /dev/null
+fi
--- src/entrypoints/entrypoint-sniper.sh
+++ src/entrypoints/entrypoint-sniper.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 echo "[NVMSIM][INFO ] Sniper Docker entrypoint"
-cd /opt/sniper || exit 0
-make -j $(nproc)
-/opt/sniper/nvmsim
-/opt/sniper/run-sniper -d /mnt/nvmsim/logs -c /opt/sniper/config/nvmsim.cfg -- /opt/sniper/nvmsim
-#/opt/sniper/run-sniper -d /mnt/nvmsim/logs -c /opt/sniper/config/nvmsim.cfg -- /bin/ls
+
+ENABLE_SNIPER=${ENABLE_SNIPER:-1}
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW="\033[0;33m"
+NOCOLOR='\033[0m'
+
+if [ $ENABLE_SNIPER == 1 ]; then
+    cd /opt/sniper || exit 0
+    make -j $(nproc)
+    /opt/sniper/nvmsim
+    /opt/sniper/run-sniper -d /mnt/nvmsim/logs -c /opt/sniper/config/nvmsim.cfg -- /opt/sniper/nvmsim
+    #/opt/sniper/run-sniper -d /mnt/nvmsim/logs -c /opt/sniper/config/nvmsim.cfg -- /bin/ls
+else
+    echo -e "[NVMSIM][${YELLOW}WARN ${NOCOLOR}] Sniper Docker entrypoint disabled"
+    tail -f /dev/null
+fi

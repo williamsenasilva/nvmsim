34a32a252df565d62966d0f2d0663d3754827cc1
William Sena Silva <williamsenasilva@yahoo.com>
2021-10-24 22:03:25 -0300

    Geração de gráfica refeita utilizando apenas matplotlib. Gráfico com barras de erros sendo gerado automaticamente. Pendente por erros calculados via bootstrap.

--- tools/generate-graph.py
+++ tools/generate-graph.py
@@ -8,9 +8,9 @@ import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import seaborn as sns
 
-benchmark_names = ["astar","bwaves","bzip2","gamess","gcc","gobmk","h264ref","hmmer","libquantum","mcf","omnetpp","perlbench","sjeng","specrand","xalancbmk"]
+#benchmark_names = ["astar","bwaves","bzip2","gamess","gcc","gobmk","h264ref","hmmer","libquantum","mcf","omnetpp","perlbench","sjeng","specrand","xalancbmk"]
+benchmark_names = ["astar","gamess","gobmk","h264ref","hmmer","libquantum","mcf","omnetpp","sjeng","specrand","xalancbmk"]
 memories = ["2D_DRAM","3D_DRAM","PCM","RRAM","STTRAM"]
 
 class Benchmark:
@@ -119,7 +119,7 @@ class Simulation:
             time_mean = time_mean / meanings
             latency_mean = latency_mean / meanings
             print("  mean time: {}\n  mean ltcy: {}\n".format(time_mean, round(latency_mean,2)))
-            line = [benchmark.name, "2D_RAM", time_mean, latency_mean]
+            line = [benchmark.name, "2D_RAM", time_mean, latency_mean, 1, 1] # TODO: atualizar 1,1 (erros) após cálculo bootstrap
             self.dataframe.append(line)
 
             time_mean = 0
@@ -134,7 +134,7 @@ class Simulation:
             time_mean = time_mean / meanings
             latency_mean = latency_mean / meanings
             print("  mean time: {}\n  mean ltcy: {}\n".format(time_mean, round(latency_mean,2)))
-            line = [benchmark.name, "3D_RAM", time_mean, latency_mean]
+            line = [benchmark.name, "3D_RAM", time_mean, latency_mean, 1, 1] # TODO: atualizar 1,1 (erros) após cálculo bootstrap
             self.dataframe.append(line)
 
             time_mean = 0
@@ -149,7 +149,7 @@ class Simulation:
             time_mean = time_mean / meanings
             latency_mean = latency_mean / meanings
             print("  mean time: {}\n  mean ltcy: {}\n".format(time_mean, round(latency_mean,2)))
-            line = [benchmark.name, "PCM", time_mean, latency_mean]
+            line = [benchmark.name, "PCM", time_mean, latency_mean, 1, 1] # TODO: atualizar 1,1 (erros) após cálculo bootstrap
             self.dataframe.append(line)
 
             time_mean = 0
@@ -164,7 +164,7 @@ class Simulation:
             time_mean = time_mean / meanings
             latency_mean = latency_mean / meanings
             print("  mean time: {}\n  mean ltcy: {}\n".format(time_mean, round(latency_mean,2)))
-            line = [benchmark.name, "RRAM", time_mean, latency_mean]
+            line = [benchmark.name, "RRAM", time_mean, latency_mean, 1, 1] # TODO: atualizar 1,1 (erros) após cálculo bootstrap
             self.dataframe.append(line)
 
             time_mean = 0
@@ -179,7 +179,7 @@ class Simulation:
             time_mean = time_mean / meanings
             latency_mean = latency_mean / meanings
             print("  mean time: {}\n  mean ltcy: {}\n".format(time_mean, round(latency_mean,2)))
-            line = [benchmark.name, "STTRAM", time_mean, latency_mean]
+            line = [benchmark.name, "STTRAM", time_mean, latency_mean, 1, 1] # TODO: atualizar 1,1 (erros) após cálculo bootstrap
             self.dataframe.append(line)
     
     def normalize_time_values(self):
@@ -194,50 +194,118 @@ class Simulation:
         print(self.dataframe)
 
 def generate_latency_graph(simulation):
-    dataframe = pd.DataFrame(simulation.dataframe, columns=["benchmark","memory", "time", "latency"])
+    df = pd.DataFrame(simulation.dataframe, columns=["benchmark","memory", "time", "latency", "time-error", "latency-error"])
     
-    palette ={"2D_RAM": "#0074D9", "3D_RAM": "#FF4136", "PCM": "#FFDC00", "RRAM": "#2ECC40", "STTRAM": "#FF851B"}
+    memory_2dram_means = df.loc[df['memory'] == '2D_RAM']["latency"].tolist()
+    memory_2dram_error = df.loc[df['memory'] == '2D_RAM']["latency-error"].tolist()
+    memory_3dram_means = df.loc[df['memory'] == '3D_RAM']["latency"].tolist()
+    memory_3dram_error = df.loc[df['memory'] == '3D_RAM']["latency-error"].tolist()
+    memory_pcm_means = df.loc[df['memory'] == 'PCM']["latency"].tolist()
+    memory_pcm_error = df.loc[df['memory'] == 'PCM']["latency-error"].tolist()
+    memory_rram_means = df.loc[df['memory'] == 'RRAM']["latency"].tolist()
+    memory_rram_error = df.loc[df['memory'] == 'RRAM']["latency-error"].tolist()
+    memory_sttram_means = df.loc[df['memory'] == 'STTRAM']["latency"].tolist()
+    memory_sttram_error = df.loc[df['memory'] == 'STTRAM']["latency-error"].tolist()
     
-    graph = sns.barplot(x="benchmark", y="latency", hue="memory", data=dataframe, palette=palette)
+    # pontos do eixo x
+    x = np.arange(len(benchmark_names))
     
-    graph.set_title("Latência média de execução", weight='bold')
-    graph.set_ylabel('LATÊNCIA (ns)', weight='bold')
-    graph.set(ylim=(0, 500))
-    graph.set_xlabel('')
-    
-    # rotations
-    # plt.xticks(rotation=45)
-    for container in graph.containers:
-        graph.bar_label(container, fmt='%.2f', padding=2, rotation=75, size=7.5)
+    # seta a largura das barras
+    width = 0.15
 
-    plt.legend(title='', loc='best')
-    graph.grid(axis="y", color='black', linewidth=0.075)
+    fig, ax = plt.subplots()
+
+    bars_1 = ax.bar(x - width*2, memory_2dram_means, width, yerr=memory_2dram_error, label="2D_RAM", color="#0d6efd", edgecolor="white")
+    bars_2 = ax.bar(x - width*1, memory_3dram_means, width, yerr=memory_3dram_error, label="3D_RAM", color="#dc3545", edgecolor="white")
+    bars_3 = ax.bar(x          , memory_pcm_means, width, yerr=memory_pcm_error, label="PCM", color="#ffc107", edgecolor="white")
+    bars_4 = ax.bar(x + width*1, memory_rram_means, width, yerr=memory_rram_error, label="RRAM", color="#20c997", edgecolor="white")
+    bars_5 = ax.bar(x + width*2, memory_sttram_means, width, yerr=memory_sttram_error, label="STTRAM", color="#fd7e14", edgecolor="white")
+
+    # insere as legendas formatados
+    ax.set_ylabel('LATÊNCIA (ns)', weight='bold')
+    ax.set_title('Latência média de execução', weight='bold')
+    ax.set(ylim=(0, 500))
+    ax.set_xticks(x)
+    ax.set_xticklabels(benchmark_names)
+    ax.legend(title='', loc='best')
+
+    # insere os valores no top odas barras
+    ax.bar_label(bars_1, fmt='%.2f', padding=2, rotation=75, size=7.5)
+    ax.bar_label(bars_2, fmt='%.2f', padding=2, rotation=75, size=7.5)
+    ax.bar_label(bars_3, fmt='%.2f', padding=2, rotation=75, size=7.5)
+    ax.bar_label(bars_4, fmt='%.2f', padding=2, rotation=75, size=7.5)
+    ax.bar_label(bars_5, fmt='%.2f', padding=2, rotation=75, size=7.5)
+
+    # cria o grid apenas em cima do eixo y com linhas cinzas e finas
+    plt.grid(axis="y", color='gray', linewidth=0.075)
+
+    # remove as linhas do topo e da direita do quadro do gráfico
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+    # maximiza a imagem
     mng = plt.get_current_fig_manager()
     mng.resize(*mng.window.maxsize())
+
     plt.show()
+
 
 def generate_time_graph(simulation):
-    dataframe = pd.DataFrame(simulation.dataframe, columns=["benchmark","memory", "time", "latency"])
+    df = pd.DataFrame(simulation.dataframe, columns=["benchmark", "memory", "time", "latency", "time-error", "latency-error"])
     
-    palette ={"2D_RAM": "#0074D9", "3D_RAM": "#FF4136", "PCM": "#FFDC00", "RRAM": "#2ECC40", "STTRAM": "#FF851B"}
+    memory_2dram_means = df.loc[df['memory'] == '2D_RAM']["time"].tolist()
+    memory_2dram_error = df.loc[df['memory'] == '2D_RAM']["time-error"].tolist()
+    memory_3dram_means = df.loc[df['memory'] == '3D_RAM']["time"].tolist()
+    memory_3dram_error = df.loc[df['memory'] == '3D_RAM']["time-error"].tolist()
+    memory_pcm_means = df.loc[df['memory'] == 'PCM']["time"].tolist()
+    memory_pcm_error = df.loc[df['memory'] == 'PCM']["time-error"].tolist()
+    memory_rram_means = df.loc[df['memory'] == 'RRAM']["time"].tolist()
+    memory_rram_error = df.loc[df['memory'] == 'RRAM']["time-error"].tolist()
+    memory_sttram_means = df.loc[df['memory'] == 'STTRAM']["time"].tolist()
+    memory_sttram_error = df.loc[df['memory'] == 'STTRAM']["time-error"].tolist()
     
-    graph = sns.barplot(x="benchmark", y="time", hue="memory", data=dataframe, palette=palette)
+    # pontos do eixo x
+    x = np.arange(len(benchmark_names))
     
-    graph.set_title("Tempo médio de execução", weight='bold')
-    graph.set_ylabel('TEMPO (ns) / 2DRAM', weight='bold')
-    graph.set(ylim=(0, 5.5))
-    graph.set_xlabel('')
-    
-    # rotations
-    # plt.xticks(rotation=45)
-    for container in graph.containers:
-        graph.bar_label(container, fmt='%.2f', padding=2, rotation=75, size=7.5)
+    # seta a largura das barras
+    width = 0.15
 
-    plt.legend(title='', loc='best')
-    graph.grid(axis="y", color='black', linewidth=0.075)
+    fig, ax = plt.subplots()
+
+    bars_1 = ax.bar(x - width*2, memory_2dram_means, width, yerr=memory_2dram_error, label="2D_RAM", color="#0d6efd", edgecolor="white")
+    bars_2 = ax.bar(x - width*1, memory_3dram_means, width, yerr=memory_3dram_error, label="3D_RAM", color="#dc3545", edgecolor="white")
+    bars_3 = ax.bar(x          , memory_pcm_means, width, yerr=memory_pcm_error, label="PCM", color="#ffc107", edgecolor="white")
+    bars_4 = ax.bar(x + width*1, memory_rram_means, width, yerr=memory_rram_error, label="RRAM", color="#20c997", edgecolor="white")
+    bars_5 = ax.bar(x + width*2, memory_sttram_means, width, yerr=memory_sttram_error, label="STTRAM", color="#fd7e14", edgecolor="white")
+
+    # insere as legendas formatados
+    ax.set_title("Tempo médio de execução", weight='bold')
+    ax.set_ylabel('TEMPO (ns) / 2DRAM', weight='bold')
+    ax.set(ylim=(0, 5.5))
+    ax.set_xticks(x)
+    ax.set_xticklabels(benchmark_names)
+    ax.legend(title='', loc='best')
+
+    # insere os valores no top odas barras
+    ax.bar_label(bars_1, fmt='%.2f', padding=2, rotation=75, size=7.5)
+    ax.bar_label(bars_2, fmt='%.2f', padding=2, rotation=75, size=7.5)
+    ax.bar_label(bars_3, fmt='%.2f', padding=2, rotation=75, size=7.5)
+    ax.bar_label(bars_4, fmt='%.2f', padding=2, rotation=75, size=7.5)
+    ax.bar_label(bars_5, fmt='%.2f', padding=2, rotation=75, size=7.5)
+
+    # cria o grid apenas em cima do eixo y com linhas cinzas e finas
+    plt.grid(axis="y", color='gray', linewidth=0.075)
+
+    # remove as linhas do topo e da direita do quadro do gráfico
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+    # maximiza a imagem
     mng = plt.get_current_fig_manager()
     mng.resize(*mng.window.maxsize())
+
     plt.show()
+
 
 if __name__ == '__main__':
     simulations = []
@@ -269,5 +337,6 @@ if __name__ == '__main__':
     
     united_simulation.make_dataframe()
     united_simulation.normalize_time_values()
-    generate_latency_graph(united_simulation)
+
     generate_time_graph(united_simulation)
+    generate_latency_graph(united_simulation)
